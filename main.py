48b7b995c05a6414baaad6914aff88ff280676b0
Ilkay Şafak Baytar <ilkaybaytar@aol.com>
2022-11-30 15:25:43 +0300

    initial commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import streamlit as st
 from streamlit_option_menu import option_menu
+from datetime import date
 
 st.set_page_config(layout="wide")
 d1, d2,d3 = st.columns(3)
@@ -11,25 +12,26 @@ with d1:
     ('LIDER.IS', 'BRSAN.IS', 'IPEKE.IS')
 )
 
+
 with d2: 
     st.date_input('Başlangıç Tarihi')
 with d3:
-    st.date_input('Bitiş Tarihi')
+    st.date_input('Bitiş Tarihi', max_value=date.today())
     
 selected = option_menu(
         menu_title = None,
-        options =["Temel Analiz", "Grafikler", "ZS Analiz"],
+        options =["Temel Analiz", "Grafikler", "ZS Analizi"],
         icons=["bar-chart", "graph-up", "clock-history"],
         orientation = "horizontal",
         default_index = 1
 )
 
 if selected == "Temel Analiz":
-    st.title(f"You have selected {selected}")
+    st.title(f"{selected} Verileri")
 if selected == "Grafikler":
-    st.title(f"You have selected {selected}")
-if selected == "contact":
-    st.title(f"You have selected {selected}")
+    st.title(f"{selected}")
+if selected == "ZS Analizi":
+    st.title("Zaman Serisi Analizi")
 
 
 
