6dcc01a9e19db797445733f900f33305bd46f763
Ilkay Şafak Baytar <ilkaybaytar@aol.com>
2022-12-01 17:35:17 +0300

    verilerin frontend ile bağlantısı ve sembol listesinin düzenlenmesi

--- main.py
+++ main.py
@@ -2,41 +2,57 @@
 
 import streamlit as st
 from streamlit_option_menu import option_menu
-from sembol_listesi import semboller
 from data import get_dataset
 from data import get_dividends
+from data import semboller
+
 
 st.set_page_config(layout="wide")
-d1, d2,d3 = st.columns(3)
+d1,d2,d3,d4 = st.columns(4)
+
 with d1:
     hisse = st.selectbox(
     "Hisse Adı",
     (semboller)
 )
 
-
-with d2: 
+with d2:
+    frekans = st.selectbox(
+        "Frekans",
+        # 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+        ("Günlük", "5 Günlük", "Haftalık", "3 Aylık")
+    )
+with d3: 
     st.date_input('Başlangıç Tarihi')
-with d3:
+with d4:
     st.date_input('Bitiş Tarihi')
+
+
     
 selected = option_menu(
         menu_title = None,
-        options =["Veriler","Temel Analiz", "Grafikler", "ZS Analiz", "Makine Öğrenmesi"],
-        icons=["bookshelf","bar-chart", "graph-up", "clock-history", "cpu"],
+        options =["Veriler","Temel Analiz", "Grafikler", "ZS Analiz", "Makine Öğrenmesi", "Derin Öğrenme"],
+        icons=["bookshelf","bar-chart", "graph-up", "clock-history", "cpu", "gpu-card"],
         orientation = "horizontal",
-        default_index = 1
+        default_index = 0
 )
+
 if selected == "Veriler":
-    st.write(f"{hisse}.IS SEÇİLDİ")
+    st.subheader(f'{hisse} Açılış Kapanış Hacim Verileri')
     df = get_dataset(f"{hisse}.IS")
     df=df.sort_index(ascending=False)
     st.dataframe(df, use_container_width=True)
+
+    st.subheader('Yıllara Göre Temettü')
+    df_dd = get_dividends(f"{hisse}.IS")
     
+    df_dd=df_dd.sort_index(ascending=False)
+    st.table(df_dd)
 if selected == "Temel Analiz":
     st.title(f"You have selected {selected}")
 if selected == "Grafikler":
-    st.title(f"You have selected {selected}")
+    df = get_dataset(f"{hisse}.IS")
+    st.line_chart(df)
 if selected == "ZS Analiz":
     st.title(f"You have selected {selected}")
 if selected == "Yapay Zeka":
