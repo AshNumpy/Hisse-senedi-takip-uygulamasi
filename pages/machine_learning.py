ce91a3df71296d8664fb34596d01ad4c15a97d7d
Ash <ramazan.erduran@hacettepe.edu.tr>
2022-12-01 17:33:01 +0300

    makine öğrenmesi ile tahminleme

--- pages/machine_learning.py
+++ pages/machine_learning.py
@@ -2,21 +2,12 @@ import pandas as pd
 import numpy as np 
 import streamlit as st
 
-# don't show info messages
-import logging
-logger = logging.getLogger('cmdstanpy')
-logger.addHandler(logging.NullHandler())
-logger.propagate = False
-logger.setLevel(logging.CRITICAL)
-
 # -- Modules --
 import sys
-sys.path.insert(0, './') # Kullanılmak istenilenn modülün uzantısı
+sys.path.insert(0, '../') # Kullanılmak istenilenn modülün uzantısı
 from data import get_dataset, get_dividends
 
-df = get_dataset("EREGL.IS", period="3y")
-
-def forecasting(df, date_column, value_column, train_size=0.75, freq='D', prediction_periods=30, country_code='TR'):
+def forecasting(df, date_column, value_column, train_size=0.85, freq='d', prediction_periods=365, country_code='TR'):
     """"
     Inputs:\n
     df: dataframe\n
@@ -40,7 +31,7 @@ def forecasting(df, date_column, value_column, train_size=0.75, freq='D', predic
     # importing the librarie(s)
     from prophet import Prophet
     import logging
-    logging.getLogger('fbprophet').setLevel(logging.WARNING)    
+    logging.getLogger('fbprophet').setLevel(logging.WARNING)
 
     # prepare the df for forecasting
     df = df.reset_index()
@@ -66,27 +57,65 @@ def forecasting(df, date_column, value_column, train_size=0.75, freq='D', predic
     
     return model, forecast, train, test
 
-model, forecast, train, test = forecasting(df, date_column='Date', value_column='Close')
 
-forecast = forecast[['ds', 'yhat_lower', 'yhat_upper', 'yhat']]
-forecast = forecast.sort_values(by='ds', ascending=False)
+def machine_learning(item='EREGL.IS', period="1y", train_size=.85, country_code='TR'):
+    import matplotlib.pyplot as plt
+    import mpld3
+    import streamlit.components.v1 as components
+    
+    st.set_page_config(layout="wide")
+    
+    df = get_dataset(item, period=period)
+    
+    model, forecast, train, test = forecasting(df, date_column='Date', value_column='Close',
+                                               prediction_periods=60, train_size=train_size, country_code=country_code)
+    
+    with st.container():
+        col1, col2 = st.columns(2)
+        with col1:
+            st.subheader('Tahmin Sonuçları Grafiği')
+            
+            fig = model.plot(forecast)
+            fig.set_size_inches(16,9)
+            fig.legend(loc='upper left', fontsize='large')
+            st.pyplot(fig)
+            
+        with col2:
+            st.subheader("Component Grafikleri")
+            
+            fig = model.plot_components(forecast)
+            fig.set_size_inches((13,9))
+            fig.tight_layout(h_pad=5)
+            
+            st.pyplot(fig)
+        
+        forecast = forecast[['ds','yhat_lower', 'yhat_upper', 'yhat']]
+        forecast = forecast.sort_values(by='ds', ascending=False)
+        
+        st.write("---")
+    
+    with st.container():
+        col1 , col2 = st.columns(2)
+        
+        with col1:
+            st.subheader("Tahmin Sonuçları")
+            st.dataframe(forecast, use_container_width=1)
+            
+        with col2:
+            test = test.sort_index(ascending=False)
 
-# fig = model.plot(forecast)
-# fig.legend(loc='upper left', fontsize='large')
-# plt.show()
+            y_pred = forecast[forecast['ds'].isin(test['ds'])][['ds', 'yhat']]
+    
+            from sklearn.metrics import mean_absolute_percentage_error
+            MAPE = mean_absolute_percentage_error(y_pred['yhat'] , test['y'])
 
-# fig = model.plot_components(forecast)
-# fig.tight_layout(h_pad=5)
-# plt.show()
+            st.markdown("<h3 style='text-align: center; color: lightblue;'>Tahmin Sonuçlarının Metrikleri</h3>", unsafe_allow_html=True)
+            
+            st.metric("MAPE",
+                  value=f"{np.round(MAPE,2)*100:.2f}%", delta=f"{np.round(-MAPE,4)*100:.2f}%")
+                
+            st.metric("Accuracy Rate",
+                    value=f"{np.round(1- MAPE,2)*100:.2f}%", delta=f"{np.round(1 - MAPE,4)*100:.2f}%")
 
 
-# Accuracy 
-y_pred = forecast.iloc[:test.shape[0],-1]
-print(forecast.shape)
-print(test.shape)
-print(y_pred.shape)
-
-from sklearn.metrics import mean_absolute_percentage_error
-
-MAPE = mean_absolute_percentage_error(y_pred, test['y'])
-print(f"MAPE: {np.round(MAPE,2)*100:.2f}")
+machine_learning()
