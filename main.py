a0b7a28aab315c0899d3daa5af941f52c58337c4
Ilkay Şafak Baytar <ilkaybaytar@aol.com>
2022-12-01 15:35:59 +0300

    dataframe oluşturulması

--- main.py
+++ main.py
@@ -3,14 +3,17 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from sembol_listesi import semboller
+from data import get_dataset
+from data import get_dividends
 
 st.set_page_config(layout="wide")
 d1, d2,d3 = st.columns(3)
 with d1:
-    st.selectbox(
+    hisse = st.selectbox(
     "Hisse Adı",
     (semboller)
 )
+
 
 with d2: 
     st.date_input('Başlangıç Tarihi')
@@ -25,7 +28,11 @@ selected = option_menu(
         default_index = 1
 )
 if selected == "Veriler":
-    st.title(f"You have selected {selected}")
+    st.write(f"{hisse}.IS SEÇİLDİ")
+    df = get_dataset(f"{hisse}.IS")
+    df=df.sort_index(ascending=False)
+    st.dataframe(df, use_container_width=True)
+    
 if selected == "Temel Analiz":
     st.title(f"You have selected {selected}")
 if selected == "Grafikler":
